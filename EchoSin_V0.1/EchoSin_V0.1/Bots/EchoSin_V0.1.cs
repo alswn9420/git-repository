9a4d5b211ee8d10547ba4a1503b390f24210d4a2
hjp0503 <60416651+hjp0503@users.noreply.github.com>
2020-07-08 19:58:08 +0900

    이미지 추가 및 시작 메세지 변경

--- EchoSin_V0.1/EchoSin_V0.1/Bots/EchoSin_V0.1.cs
+++ EchoSin_V0.1/EchoSin_V0.1/Bots/EchoSin_V0.1.cs
@@ -14,7 +14,7 @@ namespace EchoSin_V0._1.Bots
     public class EchoBot : ActivityHandler
     {
 
-        //메시지 작업이 수신됨: 메시지 작엄을 처리하도록 재정의함
+         //메시지 작업이 수신됨: 메시지 작엄을 처리하도록 재정의함
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
             var Text = turnContext.Activity.Text;
@@ -26,11 +26,11 @@ namespace EchoSin_V0._1.Bots
             //해당 reply가 있는 경우 -> reply
             if (Text.Equals("안녕"))
             {
-                await turnContext.SendActivityAsync(MessageFactory.Text("안녕하세요, 신세권 팀 입니다.\r\n 신세권은 신조어와 역세권을 합친말로, 흔히 접하는 신조어들을 손쉽게 찾을 수 있다는 의미입니다. "), cancellationToken);
+                await turnContext.SendActivityAsync(MessageFactory.Text("안녕하세요, '별다줄봇'을 기획한 신세권 팀 입니다.\r\n 신세권은 신조어와 역세권을 합친말로, 흔히 접하는 신조어들을 손쉽게 찾을 수 있다는 의미입니다. \r\n"), cancellationToken);
             }
             else if (Text.Equals("목적 알려주세요"))
             {
-                await turnContext.SendActivityAsync(MessageFactory.Text("우리는 신조어가 어색한 사람들에게 신조어를 알려주고자 \r\n 신조어 알리미 챗봇을 만들고 있습니다."), cancellationToken);
+                await turnContext.SendActivityAsync(MessageFactory.Text("저희는 신조어가 어색한 사람들에게 신조어를 알려주고자 \r\n 신조어 알리미 챗봇을 만들고 있습니다."), cancellationToken);
             }
             else if (Text.Equals("도움말"))
             {
@@ -61,16 +61,24 @@ namespace EchoSin_V0._1.Bots
             }
         }
 
-        private static async Task SendSuggestedActionsAsync(ITurnContext turnContext, CancellationToken cancellationToken, int function)
+        private static async Task SendSuggestedActionsAsync(ITurnContext turnContext, CancellationToken cancellationToken, int function) //버튼
         {
-
 
             switch (function)
             {
 
-                case 0://봇이외의 멤버가 대화에 참가함
+                case 0://봇이외의 멤버가 대화에 참가함 ㄴ=> 맨 처음에 띄워짐
 
-                    var welcomeText = turnContext.Activity.CreateReply("Hello and welcome!");
+                    //이미지
+                    var image = ProcessInput(turnContext);
+
+                    image.Text = "'별다줄봇'을 추가해주셔서 감사합니다.";
+                    image.Attachments = new List<Attachment>(){ GetInternetAttachment() };
+                    await turnContext.SendActivityAsync(image, cancellationToken);
+
+
+
+                    var welcomeText = turnContext.Activity.CreateReply("별걸 다 줄이는 요즘 말, 궁금하지 않으신가요? \r\n 저희가 알려드릴게요!");
                     welcomeText.SuggestedActions = new SuggestedActions()
                     {
                         Actions = new List<CardAction>(){
@@ -82,7 +90,7 @@ namespace EchoSin_V0._1.Bots
                     await turnContext.SendActivityAsync(welcomeText, cancellationToken);
                     break;
 
-                case 1://도움말
+                case 1://도움말 => 도움말 버튼을 누르거나 "도움말"이라고 채팅이 오면 띄워짐
                     var binText = turnContext.Activity.CreateReply("채팅창에 \"안녕\", \"목적 알려주세요\", \"도움말\"을 입력해보세요!");
                     binText.SuggestedActions = new SuggestedActions()
                     {
@@ -98,6 +106,24 @@ namespace EchoSin_V0._1.Bots
             }
 
         }
+        private static Activity ProcessInput(ITurnContext turnContext) //이미지를 보내기 위해서 별도의 Activity 객체를 만들기위한 작업
+        {
+            var activity = turnContext.Activity;
+            var reply = activity.CreateReply();
+
+            return reply;
+        }
+        private static Attachment GetInternetAttachment() //
+        {
+            // ContentUrl must be HTTPS.
+            return new Attachment
+            {
+                Name = @"2iAKkh.jpgresize.png",
+                ContentType = "image/png",
+                ContentUrl = "https://ifh.cc/g/2iAKkh.jpgresize.png",
+            };
+        }
+
 
     }
 
